edf2170ba1590e4868ffbb804e3bee6a4e0207c7
tsinh <tsinh1998@gmail.com>
2023-05-16 20:12:50 +0700

    date16.05.2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        move3: {
+          '0%': { transform: 'translate(0)' },
+          '50%': { transform: 'translate(30px)' },
+          '100%': { transform: 'translate(0px)' },
+        },
+        move4: {
+          '0%': { transform: 'translate(0)' },
+          '50%': { transform: 'translate(-30px)' },
+          '100%': { transform: 'translate(0px)' },
+        },
+        move5: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-30px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+      }
+    },
   },
   plugins: [],
 }
