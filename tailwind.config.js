b429787a9ac3d1e5aa2fe8d0ff89fc50fc67df0a
tsinh <tsinh1998@gmail.com>
2023-05-26 22:36:05 +0700

    date26.5.2023
    a
    dasfe
    
    git commit -m date26.5.2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,18 +20,30 @@ module.exports = {
           '50%': { transform: 'translateY(-30px)' },
           '100%': { transform: 'translateY(0px)' },
         },
-        rainbow: {
-          '0%': { background_position: 'left' },
-          '50%': { background_position: 'right' },
-          '100%': { background_position: 'left' },
-        },
         watermarkanimate: {
           '0%': { left: '0' },
           '100%': { left: '90%' },
         },
+        slidein: {
+          '0%': { opacity:'0' ,transform: 'translateY(-100%)' },
+          '60%': { opacity:'1' ,transform: 'translateY(20%)' },
+          '100%': { opacity:'1' ,transform: 'translateY(0%)' },
+        },
+        slideout: {
+          '0%': { opacity:'1' ,transform: 'translateY(0%)' },
+          '60%': { opacity:'0' ,transform: 'translateY(120%)' },
+          '100%': { opacity:'0' ,transform: 'translateY(100%)' },
+        },
+        cdrotate3in: {
+          '0%': { transform: 'rotateY(180deg)' },
+          '100%': { transform: 'rotateY(0deg)' },
+        },
+        cdrotate3out: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(-180deg)' },
+        }
       }
     },
   },
   plugins: [],
 }
-
