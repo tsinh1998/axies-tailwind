d5167cc9014b136e9db222ee4ad6ad04c6852c98
tsinh <tsinh1998@gmail.com>
2023-06-05 21:02:14 +0700

    date5.6.2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
-  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: 'class',
   theme: {
     extend: {
       keyframes: {
